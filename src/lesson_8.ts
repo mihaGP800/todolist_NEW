79f85cf2ded33133365871665857db29a53985ec
mihaGP800 <mihagoroh@gmail.com>
2022-03-25 08:46:14 +0300

    lesson_8.test

---
+++ src/lesson_8.ts
@@ -0,0 +1,95 @@
+// 1. Функция sum принимает параметром целые положительные
+// числа (неопределённое кол-во) и возвращает их сумму (rest).
+
+export function sum(...nums: Array<number>): number {
+    // console.log(nums)
+    return nums.reduce((acc, n) => acc + n)
+}
+
+
+// 2. Функция getTriangleType принимает три параметра:
+// длины сторон треугольника.
+// Функция должна возвращать:
+//  - "10", если треугольник равносторонний,
+//  - "01", если треугольник равнобедренный,
+//  - "11", если треугольник обычный,
+//  - "00", если такого треугольника не существует.
+
+export function getTriangleType(a: number, b: number, c: number): string {
+    if (a + b > c && b + c > a && c + a > b) {
+        if (a === b && b === c) {
+            return '10'
+        } else if (a === b || b === c || a === c) {
+            return '01'
+        } else {
+            return '11'
+        }
+    }
+    return '00'
+}
+
+
+// 3. Функция getSum принимает параметром целое число и возвращает
+// сумму цифр этого числа
+
+export function getSum(number: number): number {
+    // return String(number).split('').reduce((acc, n) => acc + Number(n), 0)
+    return JSON.stringify(number).split('').reduce((acc, n) => acc + JSON.parse(n), 0)
+}
+
+
+// 4. Функция isEvenIndexSumGreater принимает  параметром массив чисел.
+// Если сумма чисел с чётными ИНДЕКСАМИ!!! (0 как чётный индекс) больше
+// суммы чисел с нечётными ИНДЕКСАМИ!!!, то функция возвращает true.
+// В противном случае - false.
+
+export const isEvenIndexSumGreater = (arr: Array<number>): boolean => {
+    return arr.reduce((acc, n, i) => i % 2 ? acc - n : acc + n) > 0
+}
+
+// 5. Функция getSquarePositiveIntegers принимает параметром массив чисел и возвращает новый массив. 
+// Новый массив состоит из квадратов целых положительных чисел, котрые являются элементами исходгого массива.
+// Исходный массив не мутирует.
+
+
+export function getSquarePositiveIntegers(array: Array<number>): Array<number> {
+
+    return array.filter(el => el % 1 === 0 && el > 0).map(el => el ** 2)
+}
+
+// 6. Функция принимает параметром целое не отрицательное число N и возвращает сумму всех чисел от 0 до N включительно
+// Попробуйте реализовать функцию без использования перебирающих методов.
+
+export function sumFirstNumbers(N: number): number {
+
+    // let sum = 0
+    // for (let i = 0; i <= N; i++) {
+    //     sum += i
+    // }
+    // return sum
+
+    return [...Array(N)].reduce((acc, n, i) => acc + i + 1, 0)
+}
+
+// ...и "лапку" вверх!!!!
+
+
+// Д.З.:
+// 7. Функция-банкомат принимает параметром целое натуральное число (сумму).
+// Возвращает массив с наименьшим количеством купюр, которыми можно выдать эту
+// сумму. Доступны банкноты следующих номиналов:
+// const banknotes = [1000, 500, 100, 50, 20, 10, 5, 2, 1].
+// Считаем, что количество банкнот каждого номинала не ограничено
+
+
+export function getBanknoteList(amountOfMoney: number): Array<number> {
+    const banknotes = [1000, 500, 100, 50, 20, 10, 5, 2, 1]
+    let result = []
+    for (let i = 0; i < banknotes.length; i++) {
+        while (amountOfMoney - banknotes[i] >=0){
+            amountOfMoney -=banknotes[i]
+            result.push(banknotes[i])
+        }
+    }
+    return result
+}
